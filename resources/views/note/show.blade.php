13e1a8b2c9dbb6da348181433db4e878a4f0f6a3
Mario Capitbrok <mario@capitbrok.com>
2024-07-28 21:33:06 -0600

    Define HTML for other endpoints

--- resources/views/note/show.blade.php
+++ resources/views/note/show.blade.php
@@ -1,3 +1,14 @@
 <x-layout>
-    <h1>show</h1>
+    <div class="note-container single-note">
+        <h1>Note: {{ $note->created_at }}</h1>
+        <div class="note-buttons">
+            <a href="{{ route('note.edit', $note) }}" class="note-edit-button">Edit</a>
+            <button class="note-delete-button">Delete</button>
+        </div>
+    </div>
+    <div class="note">
+        <div class="note-body">
+            {{ $note->note }}
+        </div>
+    </div>
 </x-layout>
